fecc35cf7f2e215d9b7b6382b5cb66aae1613a0d
Gamaliel Widhi Pradana <board.dabil@gmail.com>
2023-06-23 18:17:07 +0700

    Fix foods page

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,7 +5,7 @@
       <div class="col-md-6 align-self-center">
         <h2><strong>Delicious Food Menu, </strong><br />in Your Gadget</h2>
         <p>Ayo segera pilih dan pesan makanan favorit Anda</p>
-        <router-link to="/" class="btn btn-lg btn-success"><i class="bi bi-arrow-right"></i> Pesan</router-link>
+        <router-link to="/foods" class="btn btn-lg btn-success"><i class="bi bi-arrow-right"></i> Pesan</router-link>
       </div>
       <div class="col-md-6">
         <img :src="'assets/img/svg/hero.svg'" class="img-fluid hero-img" />
@@ -22,7 +22,7 @@
       <div class="col-md-6 align-self-center">
         <h2><strong>Delicious Food Menu, </strong><br />in Your Gadget</h2>
         <p>Ayo segera pilih dan pesan makanan favorit Anda</p>
-        <router-link to="/" class="btn btn-lg btn-success"><i class="bi bi-arrow-right"></i> Pesan</router-link>
+        <router-link to="/foods" class="btn btn-lg btn-success"><i class="bi bi-arrow-right"></i> Pesan</router-link>
       </div>
     </div>
   </div>
--- src/views/Foods.vue
+++ src/views/Foods.vue
@@ -1,16 +1,55 @@
 <template>
     <Navbar />
-    <h2>Daftar Makanan</h2>
+    <div class="container mt-4">
+        <h2>Daftar <strong>Makanan</strong></h2>
+        <div class="input-group mt-3">
+            <input v-model="search" @keyup="searchFood" type="text" class="form-control" placeholder="Cari Makanan..">
+            <span class="input-group-text" id="search">
+                <i class="bi bi-search"></i>
+            </span>
+        </div>
+        <div class="row g-4 my-3">
+            <div class="col-md-4" v-for="product in products" :key="product.id">
+                <CardProduct :product="product"></CardProduct>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import CardProduct from "@/components/CardProduct.vue";
+import axios from "axios";
 
 export default {
     name: `Foods`,
     components: {
         Navbar,
+        CardProduct
     },
+    data() {
+        return {
+            products: [],
+            search: ''
+        }
+    },
+    methods: {
+        setProduct(data) {
+            this.products = data;
+        },
+        searchFood() {
+            axios
+                .get("http://localhost:3000/products?q=" + this.search)
+                .then((response) => this.setProduct(response.data))
+                .catch((error) => console.log(error));
+        }
+    },
+    mounted() {
+        axios
+            .get("http://localhost:3000/products")
+            .then((response) => this.setProduct(response.data))
+            .catch((error) => console.log(error));
+    }
 };
 </script>
 
